62812a1fc451ec90df2cfd968ab0558d3dcde267
Nikita <37281645+grungeee@users.noreply.github.com>
2025-07-30 22:18:22 +0200

    Style modal layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,7 @@ module.exports = {
       colors: {
         green: '#8dff94',
         yellow: '#f4ff61',
-        purple: '#9a93ff',
+        purple: '#5d5899',
         'light-grey': '#6a6a6a',
         'light-grey-char': '#444444',
         'light-grey-key': '#4b4b4b',
